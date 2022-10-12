ac73c632fd78aa8576c0a552be36e998b9379466
yogesh lodha <yolodha@gmail.com>
2022-10-12 11:56:58 +0530

    data validation

--- WebAPI_App/Models/EmployeeModel.cs
+++ WebAPI_App/Models/EmployeeModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +8,11 @@ namespace WebAPI_App.Models
 {
     public class EmployeeModel
     {
+
+            [Required]
             public int Id { get; set; }
+
+            [StringLength(20, ErrorMessage = "Description Max Length is 20")]
             public string userName { get; set; }
             public System.DateTime DOB { get; set; }
             public bool IsActive { get; set; }
